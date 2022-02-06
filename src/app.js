bced8e35ccaf470cc6ed4813592e949b6a65b5bb
victorioxd <vicxd@pm.me>
2022-02-06 19:56:55 +0100

    ✨ express -> fastify

--- src/app.js
+++ src/app.js
@@ -1,17 +1,41 @@
-var express = require("express");
-const rateLimit = require("express-rate-limit");
-var app = express();
-require("dotenv").config();
-const fumosController = require("./fumos/controller")
-var db = require('./db'); 
-var RandomController = require("./random/controller");
-app.use("/random", RandomController);
-app.use("/fumos", fumosController)
-app.use("/fumo", RandomController)
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 100,
-});
-app.use(limiter)
-app.use("/", (req, res) => res.status(404).send("Here are no fumos, random fumo in /random"))
-module.exports = app;
+import fastify from 'fastify';
+import fastifyMongo from "fastify-mongodb";
+
+const app = fastify();
+
+app.register(fastifyMongo, {
+    forceClose: true,
+    url: process.env.MONGOURL
+})
+
+app.get('/', async (request, reply) => {
+    reply
+        .status(400)
+        .send("Here are no fumos, random fumo in /random")
+})
+
+app.get("/random", async(request, reply) =>{
+    this.mongo.db.collection("fumos").find().toArray((err, fumos) => {
+        const fumo = fumos[Math.floor(Math.random() * fumos.length)];
+        if(err) reply.status(500).send(err);
+        reply.status(200).send(fumo);
+    })
+})
+
+app.get("/fumos", async(request, reply) =>{
+    this.mongo.db.collection("fumos").find().toArray((err, fumos) => {
+        if(err) reply.status(500).send(err);
+        reply.status(200).send(fumos);
+    })
+})
+
+app.get("/fumos/:id", async(request, reply) =>{
+    const id = request.params['id'];
+    this.mongo.db.collection("fumos").findOne({_id: id}, (err, fumo) => {
+        if(err) reply.status(500).send(err);
+        reply.status(200).send(fumo);
+    })
+})
+
+
+export default app
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
-var app = require("./app");
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function () {
-  console.log("Listening on port " + port);
-});
+import app from "./app.js";
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, err =>{
+    if(err) throw err;
+    console.log(`server is listening on ${port}`);
+})
