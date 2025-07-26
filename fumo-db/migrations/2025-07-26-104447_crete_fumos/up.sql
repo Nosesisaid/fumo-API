f82dc49bba7c67d0b177415e22c35a8bdce85b77
Vic <vicxd@pm.me>
2025-07-26 13:00:03 +0200

    Create virtual workspace

---
+++ fumo-db/migrations/2025-07-26-104447_crete_fumos/up.sql
@@ -0,0 +1,11 @@
+-- Your SQL goes here
+CREATE TABLE fumos (
+    id BIGSERIAL PRIMARY KEY,
+    caption VARCHAR NOT NULL,
+    img VARCHAR NOT NULL,
+    public BOOLEAN NOT NULL DEFAULT FALSE,
+    involved TEXT[],
+    attribution TEXT,
+    submission_time TIMESTAMP NOT NULL DEFAULT NOW(),
+    submitter VARCHAR(32)
+);
---
+++ fumo-db/src/schema.rs
@@ -0,0 +1,15 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    fumos (id) {
+        id -> Int8,
+        caption -> Varchar,
+        img -> Varchar,
+        public -> Bool,
+        involved -> Nullable<Array<Nullable<Text>>>,
+        attribution -> Nullable<Text>,
+        submission_time -> Timestamp,
+        #[max_length = 32]
+        submitter -> Nullable<Varchar>,
+    }
+}
