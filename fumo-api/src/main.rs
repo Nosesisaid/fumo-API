d5db6e8f7539aa9c35d4b90bf80c700985da0398
Vic <vicxd@pm.me>
2025-07-27 11:52:20 +0200

    Add paginated endpoint OMG i've been debugging this for like a lot of time and all i needed to do was to make the function async

--- fumo-api/src/main.rs
+++ fumo-api/src/main.rs
@@ -5,9 +5,10 @@ use std::{env, net::SocketAddr};
 use diesel::{PgConnection, QueryDsl, RunQueryDsl, SelectableHelper};
 use dotenvy::dotenv;
 use axum::{
-    extract::State, http::StatusCode, response::{IntoResponse, Json}, routing::{get, post}, Router
+    extract::{Query, State}, http::StatusCode, response::{IntoResponse, Json}, routing::{get, post}, Router, debug_handler
 };
 use fumo_db::{create_pool, models::Fumo, DbPool};
+use serde::Deserialize;
 
 
 #[derive(Clone)]
@@ -56,28 +57,49 @@ fn admin()-> Router<AppState> {
 }
 
     
-   //Test function. The DB never should return everything unpaginated 
-    async fn list_all(
-        State(state): State<AppState> 
-    ) -> impl IntoResponse {
-        let mut conn = state.db.get().expect("Failed getting a connection");
-
-        let results = fumo_db::operations::fetch_fumos(&mut conn, 0, None);
-
-        if let Ok(res)= results  {
-           Ok(Json(res))
-        } else {
-            Err(StatusCode::INTERNAL_SERVER_ERROR)
-        }
+//Test function. The DB never should return everything unpaginated 
+async fn list_all(
+    State(state): State<AppState> 
+) -> impl IntoResponse {
+    let mut conn = state.db.get().expect("Failed getting a connection");
+    let results = fumo_db::operations::fetch_fumos(&mut conn, 0, None);
+    if let Ok(res)= results  {
+       Ok(Json(res))
+    } else {
+        Err(StatusCode::INTERNAL_SERVER_ERROR)
     }
+}
 
 fn fumo() -> Router<AppState> {
 
-
-
     //todo!("All the reading stuff")
-    Router::new().route("/list_all",get(list_all))
-    
+    Router::new()
+    .route("/fumos/list_all",get(list_all))
+    .route("/fumos", get(list)) 
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize)]
+struct Pagination {
+    page: Option<u32>,
+    limit: Option<u32>,
+}
+
+async fn list(Query(pagination): Query<Pagination>,State(state): State<AppState>) -> impl IntoResponse {
+    let Ok( mut conn)= state.db.get() else {
+      return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    };
+    let page = pagination.page.unwrap_or(1);
+    let limit = pagination.limit.unwrap_or(15);
+
+    let offset = (limit * page) - limit;
+
+    let results = fumo_db::operations::fetch_fumos(&mut conn, offset.into(), Some(limit.into()));
+
+    match results {
+        Ok(r) => Ok(Json(r)),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR)
+    }
 }
 
 
