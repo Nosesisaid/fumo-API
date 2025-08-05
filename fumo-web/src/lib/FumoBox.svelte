b32ce95346d4f6e0579e9cd1b9aee7579694856e
Vic <vicxd@pm.me>
2025-08-06 00:48:12 +0200

    Fumo web basic state

--- fumo-web/src/lib/FumoBox.svelte
+++ fumo-web/src/lib/FumoBox.svelte
@@ -9,5 +9,31 @@
       src={fumo.img}
       alt={`Image of a Touhou series plush Caption: ${fumo.caption}`}
     />
-    <i>{fumo.id}</i>
+    <div class="infocolumn">
+    <t>#{fumo.id}</t>
+    <t>Featuring: <i>{(fumo.involved.length > 0 ? fumo.involved.join(" ") : "No one")}</i></t>
+    <i></i>
+    </div>
 </div>
+
+<style>
+    .infocolumn {
+        padding: 0px 10px;
+        display: flex;
+        flex-direction: column;
+    }
+    t {
+        color: white;
+    }
+    .container {
+        background-color: rgb(103, 136, 38);
+        
+        width: 250px;
+        padding: 10px;
+        flex-direction: column;
+        display: flex;
+    }
+    .container img {
+        margin: 10px 5px ;
+    }
+</style>
--- fumo-web/src/routes/+page.svelte
+++ fumo-web/src/routes/+page.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
   import type { APIFumo } from "$lib";
   import FumoBox from "$lib/FumoBox.svelte";
+  import Pagination from "$lib/Pagination.svelte";
   import { onMount } from "svelte";
 
+  let availablePages = $state(1);
+  let page = $state(1);
   let fumos: APIFumo[] | null = $state(null);
   let loading = $state(true);
 
-  onMount(async () => {
-    try {
-      const res = await fetch("https://fumo-api-staging.nosesisaid.com/fumos");
-      if (!res.ok) {
-        throw new Error("Error while fetching the fumos! "+ await res.text());
-      }
 
-      fumos = await res.json();
-    } catch (e) {
-        alert(e);
-    } finally {
-        loading = false
-    }
+  onMount(()=>{
+    fetch("https://fumo-api-staging.nosesisaid.com/fumos/count").then(r=>{
+        r.text().then(t=>{
+            availablePages = parseInt(t)
+        })
+    })
+  })
+
+  $effect(() => {
+    
+    fetch("https://fumo-api-staging.nosesisaid.com/fumos?page="+page).then(r=>{
+
+        if (!r.ok){
+            alert("Error loading the fumos"+r.status)
+        }
+        r.json().then(f => {
+            fumos = f as any
+            loading = false
+        })
+    })
+    
   });
 </script>
 
@@ -29,21 +41,42 @@
   >
 </nav>
 
+<div class="header">
+<h1>Fumo?</h1>
+<p>Learn more about the fumo-API in our GitHub. Link above.</p>
+</div>
+
+
+
 {#if loading}
 Loading...
 {:else}
 {#if fumos}
 
+<div class="fumos">
 {#each fumos as fumo}
   <FumoBox {fumo}/>
 {/each}
+</div>
+{/if}
 {/if}
 
-{/if}
+<Pagination bind:activePage={page} numberOfPages={availablePages} />
 
 <style>
+
+    .fumos {
+        margin: 0px 50px;
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+    }
+    .header {
+        padding: 40px;
+    }
     a {
         text-decoration: none;
+        color: darkkhaki;
     }
     nav {
         display: flex;
@@ -51,5 +84,6 @@ Loading...
         align-items: center;
         width: 100%;
         background-color: antiquewhite;
+        padding: 0px 15px;
     }
 </style>
